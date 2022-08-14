561e1e8cc0f90f6b921dbf84d998eead49f29dca
Patric Mariano <97797728+xsrpatric@users.noreply.github.com>
2022-08-14 12:58:47 -0300

    Delete Arquivos Flappy Bird directory

--- Arquivos Flappy Bird/Acao.java
+++
@@ -1,3 +0,0 @@
-public interface Acao {
-    void executa();
-}
--- Arquivos Flappy Bird/Jogo.java
+++
@@ -1,8 +0,0 @@
-public interface Jogo {
-    String getTitulo();
-    int getLargura();
-    int getAltura();
-    void tique(java.util.Set<String> teclas, double dt);
-    void tecla(String tecla);
-    void desenhar(Tela tela);
-}
